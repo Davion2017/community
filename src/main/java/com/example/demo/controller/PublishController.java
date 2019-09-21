0d84993b7dd13f9efc5bbecbe8d8ea331ef6b215
Davion <davion2017@foxmail.com>
2019-09-21 21:48:42 +0800

    09-21 p21 发布完成

---
+++ src/main/java/com/example/demo/controller/PublishController.java
@@ -0,0 +1,80 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.QuestionMapper;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Question;
+import com.example.demo.model.User;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model){
+
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+
+        if (title == null || title.equals("")){
+            model.addAttribute("error", "标题不能为空");
+        }
+        if (description == null || description.equals("")){
+            model.addAttribute("error", "补充不能为空");
+        }
+        if (tag == null || tag.equals("")){
+            model.addAttribute("error", "标签不能为空");
+        }
+
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                break;
+            }
+        }
+
+        if (user == null){
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+        questionMapper.create(question);
+        return "redirect:/";
+    }
+}
