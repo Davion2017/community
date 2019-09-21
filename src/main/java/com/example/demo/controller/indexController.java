325df601286b598693e639b2e26fe6b80b414b00
Davion <davion2017@foxmail.com>
2019-09-21 15:16:00 +0800

    09-21 18p 持久登录

--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -1,11 +1,33 @@
 package com.example.demo.controller;
 
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class indexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping("/")
-    public String index() { return "index"; }
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        return "index";
+    }
 }
