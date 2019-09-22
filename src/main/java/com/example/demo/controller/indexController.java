71856c1f8a3a9c2b7a6c440dbd807c014043a75e
Davion <davion2017@foxmail.com>
2019-09-22 11:36:41 +0800

    09-22 p23 首页列表完成

--- src/main/java/com/example/demo/controller/indexController.java
+++ src/main/java/com/example/demo/controller/indexController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.QuestionDTO;
+import com.example.demo.mapper.QuestionMapper;
 import com.example.demo.mapper.UserMapper;
+import com.example.demo.model.Question;
 import com.example.demo.model.User;
+import com.example.demo.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class indexController {
@@ -15,8 +21,12 @@ public class indexController {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private QuestionService questionService;
+
     @RequestMapping("/")
-    public String index(HttpServletRequest request) {
+    public String index(HttpServletRequest request,
+                        Model model) {
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0){
             for (Cookie cookie : cookies) {
@@ -30,6 +40,9 @@ public class indexController {
                 }
             }
         }
+
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions", questionList);
         return "index";
     }
 }
--- src/main/java/com/example/demo/dto/QuestionDTO.java
+++ src/main/java/com/example/demo/dto/QuestionDTO.java
@@ -1,9 +1,10 @@
-package com.example.demo.model;
+package com.example.demo.dto;
 
+import com.example.demo.model.User;
 import lombok.Data;
 
 @Data
-public class Question {
+public class QuestionDTO {
     private Integer id;
     private String title;
     private String description;
@@ -14,4 +15,5 @@ public class Question {
     private Integer commentCount;
     private Integer likeCount;
     private Integer viewCount;
+    private User user;
 }
