af827e5d0ad3e0f3f9692730af608b3bdfbf2e90
Davion <davion2017@foxmail.com>
2019-09-29 09:32:11 +0800

    p41 页面登录

--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -11,12 +11,19 @@ function post() {
             "type": 1
         }),
         success: function (response){
-            if (response.code == 200){
+            if (response.code === 200){
                 $("#comment-section").hide();
             }else {
-                alert(response.message);
+                if (response.code === 2003) {
+                    var isAccepted = confirm(response.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=cee8aa31037deae13bc8&redirect_uri=http://127.0.0.1:8080/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", "true");
+                    }
+                }else {
+                    alert(response.message);
+                }
             }
-            console.log(response);
         },
         dataType: "json"
     })
