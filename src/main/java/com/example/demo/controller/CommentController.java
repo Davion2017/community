f19d9ec8fd9ff72fdf500315dfdf0bbd9ff47fc1
Davion <davion2017@foxmail.com>
2019-09-29 17:02:04 +0800

    p43 前端页面优化

--- src/main/java/com/example/demo/controller/CommentController.java
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -6,6 +6,7 @@ import com.example.demo.exception.CustomizeErrorCode;
 import com.example.demo.model.Comment;
 import com.example.demo.model.User;
 import com.example.demo.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,12 +25,17 @@ public class CommentController {
     @ResponseBody
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
     public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
-                       HttpServletRequest request){
+                       HttpServletRequest request) {
 
-        User user = (User)request.getSession().getAttribute("user");
-        if (user == null){
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
+
+        if (commentCreateDTO == null || StringUtils.isBlank(commentCreateDTO.getContent())) {
+            return ResultDTO.errorOf(CustomizeErrorCode.COMMENT_IS_EMPTY);
+        }
+
         Comment comment = new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
         comment.setContent(commentCreateDTO.getContent());
--- src/main/resources/static/js/demo.js
+++ src/main/resources/static/js/demo.js
@@ -1,6 +1,10 @@
 function post() {
     var questionId = $("#question-id").val();
     var content = $("#comment_content").val();
+    if (!content.trim()) {
+        alert("不能回复空内容~~~");
+        return;
+    }
     $.ajax({
         type: "POST",
         url: "/comment",
@@ -10,17 +14,18 @@ function post() {
             "content": content,
             "type": 1
         }),
-        success: function (response){
-            if (response.code === 200){
-                $("#comment-section").hide();
-            }else {
+        success: function (response) {
+            if (response.code === 200) {
+                // $("#comment-section").hide();
+                window.location.reload();
+            } else {
                 if (response.code === 2003) {
                     var isAccepted = confirm(response.message);
-                    if(isAccepted){
+                    if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=cee8aa31037deae13bc8&redirect_uri=http://127.0.0.1:8080/callback&scope=user&state=1");
                         window.localStorage.setItem("closable", "true");
                     }
-                }else {
+                } else {
                     alert(response.message);
                 }
             }
